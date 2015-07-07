fc121c00d6e374349b1d4437e93b8637d6617f2c
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2015-07-07 08:57:14 -0300

    Removed CDN from about.html temporarily and added CDN for glyph icons fonts.

--- src/app.js
+++ src/app.js
@@ -6,14 +6,6 @@ var path = require('path'),
 
 var app = express();
 
-/*
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-*/
-
 app.use(cors());
 
 app.get('/', function (req, res) {
