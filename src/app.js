bc3eed193b67602823cf056ff71b09c7c2c8a15e
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2015-07-07 08:44:58 -0300

    Added CORS support.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 'use strict';
 
 var path = require('path'),
+  cors = require('cors'),
   express = require('express');
 
 var app = express();
+
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'), {
