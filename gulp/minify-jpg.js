216014c218ba3285bd2032340ffbeeab3238935b
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2015-07-14 22:42:01 -0300

    Renamed img folder to images.

--- gulp/minify-jpg.js
+++ gulp/minify-jpg.js
@@ -1,12 +1,12 @@
 module.exports = function (gulp, plugins) {
   return function () {
-    return gulp.src('./src/img/**/*.jpg')
+    return gulp.src('./src/images/**/*.jpg')
       .pipe(plugins.imagemin({
         progressive: true
       }))
       .pipe(plugins.rename(function (path) {
         path.basename += ".min";
       }))
-      .pipe(gulp.dest('./dist/img'));
+      .pipe(gulp.dest('./dist/images'));
   }
 };
--- gulp/minify-svg.js
+++ gulp/minify-svg.js
@@ -1,10 +1,10 @@
 module.exports = function (gulp, plugins) {
   return function () {
-    return gulp.src('./src/img/**/*.svg')
+    return gulp.src('./src/images/**/*.svg')
       .pipe(plugins.svgmin())
       .pipe(plugins.rename(function (path) {
         path.basename += ".min";
       }))
-      .pipe(gulp.dest('./dist/img'));
+      .pipe(gulp.dest('./dist/images'));
   }
 };
