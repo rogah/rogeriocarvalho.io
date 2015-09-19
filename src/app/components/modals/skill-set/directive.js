0e2319b2f6a1b567dad0b97dacddc130c69af880
Rogerio W. Carvalho <rogerio.w.carvalho@gmail.com>
2015-09-19 17:35:33 -0300

    Removed overlay from skillset.

--- src/app/components/modals/skill-set/directive.js
+++ src/app/components/modals/skill-set/directive.js
@@ -1,40 +1,19 @@
 var util = require('util'),
-  $ = require('jquery'),
-  TweenLite = require('gsap-tween-lite');
+  $ = require('jquery');
 
 module.exports = function () {
 
   function link(scope, element, attrs) {
-    console.log(TweenLite);
-
     var $trigger = $(util.format('#%s', attrs.triggerId)),
-      $close = $('.close a', element),
-      $overlay = $('body > #overlay');
-
-    if (!$overlay.length) {
-      $overlay = $('<div/>').attr('id', 'overlay');
-      $('body').append($overlay);
-    }
+      $close = $('.close a', element);
 
     $trigger.click(function (e) {
       element.addClass('show');
-      $overlay.addClass('show');
-    });
-
-    element.children('.close').click(function (e) {
-      element.removeClass('show');
-      $overlay.removeClass('show');
     });
 
     $close.click(function (e) {
       e.preventDefault();
       element.removeClass('show');
-      $overlay.removeClass('show');
-    });
-
-    $overlay.click(function (e) {
-      element.removeClass('show');
-      $overlay.removeClass('show');
     });
   }
 
